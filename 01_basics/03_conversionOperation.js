d0894aa69de82028be4f9c2dd6e2f5e0f6fa7143
sourav-dhankhar <79415923+sourav-dhankhar@users.noreply.github.com>
2024-01-26 17:24:38 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -46,15 +46,15 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2);    12
+// console.log(1 + "2");    12
+// console.log("1" + 2 + 2); 122
+// console.log(1 + 2 + "2");  32   because whatever the type comes first it will go according to that
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true);    1
+// console.log(+"");      0
 
 let num1, num2, num3
 
